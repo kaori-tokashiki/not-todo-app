68c41eeea7296deb9df5c76a34bccba259a0537d
kaori <kaori@weseek.co.jp>
2021-09-25 17:34:11 +0900

    apply grid

--- src/components/NtaSection.tsx
+++ src/components/NtaSection.tsx
@@ -1,7 +1,18 @@
 import { NtaCard } from './NtaCard'
+import Grid from '@mui/material/Grid';
+
 
 export const NtaSection = (): JSX.Element => {
     return (
-        <NtaCard />
+        <>
+            {/* TODO： cards' position adjustment by #11. justifyContent="center" do not work */}
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} justifyContent="center" alignItems="center">
+                {Array.from(Array(6)).map((_, index) => (
+                    <Grid item xs={2} sm={4} md={4} key={index}>
+                        <NtaCard />
+                    </Grid>
+                ))}
+            </Grid>
+        </>
     );
 }
