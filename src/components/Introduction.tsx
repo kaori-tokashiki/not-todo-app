6ba0f757de7371321f47fa6f6e9965e0f0b5e028
kaori <kaori@weseek.co.jp>
2021-09-25 20:03:18 +0900

    apply padding

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -4,7 +4,9 @@ import { Favicon } from '../components/Favicon'
 export const Introduction = (): JSX.Element => {
     return(
         <Box sx={{ py: 15 }}>
-            <Favicon isAnimated size={200} />
+            <Box sx={{ py: 5 }}>
+                <Favicon isAnimated size={200} />
+            </Box>
             <h1>What's NTA ?</h1>
             <h2>NTA(Not To Do App) is the app for the people who cannot say the things they do not want to do</h2>
             <h3><a href="#declare">Let's get started !</a></h3>
