355f2d489717e9a30b52c7ecf2a6921cde9ba55b
kaori <kaori@weseek.co.jp>
2021-09-25 18:28:30 +0900

    changed array size

--- src/components/NtaSection.tsx
+++ src/components/NtaSection.tsx
@@ -7,7 +7,7 @@ export const NtaSection = (): JSX.Element => {
         <>
             {/* TODO： cards' position adjustment by #11. justifyContent="center" do not work */}
             <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} justifyContent="center" alignItems="center">
-                {Array.from(Array(6)).map((_, index) => (
+                {Array.from(Array(9)).map((_, index) => (
                     <Grid item xs={2} sm={4} md={4} key={index}>
                         <NtaCard />
                     </Grid>
