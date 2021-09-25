1816ca234d1ad7c7abbb77bb0cf46df2eb632f29
kaori <kaori@weseek.co.jp>
2021-09-25 19:11:21 +0900

    fixed grid

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
-
+import { Box } from '@mui/material' 
 import { Introduction } from './Introduction'
 import { NtaSection } from './NtaSection'
 
 export const Home = (): JSX.Element => {
   return (
-    <>
+    <Box sx={{ px: {md: 20} }}　justifyContent="center">
         <Introduction />
         <NtaSection />
-    </>
+    </Box>
   );
 }
--- src/components/NtaSection.tsx
+++ src/components/NtaSection.tsx
@@ -6,7 +6,7 @@ export const NtaSection = (): JSX.Element => {
     return (
         <Box style={{ marginBottom: -200}}>
             {/* TODO： cards' position adjustment by #11. justifyContent="center" do not work */}
-            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} justifyContent="center" alignItems="center">
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 1, sm: 8, md: 12 }} justifyContent="center" alignItems="center">
                 {Array.from(Array(9)).map((_, index) => (
                     <Grid item xs={2} sm={4} md={4} key={index}>
                         <NtaCard />
