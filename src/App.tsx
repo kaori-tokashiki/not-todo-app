edbd1e01161a4e956c12c25fac29049f72280b17
kaori <kaori@weseek.co.jp>
2021-09-25 15:03:43 +0900

    inserting a background image

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Home } from './components/Home';
 import { NavBar } from './components/NavBar';
+import { BackgroundImage } from './components/BackgroundImage';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Home />
+      <BackgroundImage />
     </div>
   );
 }
