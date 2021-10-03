2898a6d4145b7952e4074c5984ff4d15b926b82f
kaori <kaori@weseek.co.jp>
2021-10-03 23:08:28 +0900

    fixed cors err

--- app.ts
+++ app.ts
@@ -3,6 +3,16 @@ import express from 'express'
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => res.send('Hello Worldddd!'))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Credentials', 'true')
+    next()
+})
+
+app.get('/', (req: express.Request, res: express.Response) => 
+    res.send('Hello Worldddd!'),
+)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,31 @@
+import axios from 'axios';
+
 import './App.css';
 import { Home } from './components/Home';
 import { NavBar } from './components/NavBar';
 import { BackgroundImage } from './components/BackgroundImage';
+import { useEffect } from 'react';
+
+
+const axiosBase = require('axios');
+const axiosInstance = axiosBase.create({
+  baseURL: 'http://localhost:5000', // バックエンドB のURL:port を指定する
+  headers: {
+    'Content-Type': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest'
+  },
+  responseType: 'json'  
+});
+
+
 
 function App() {
+  useEffect(() => {
+    const res = axiosInstance.get('/')
+    console.log(res)
+  }, [])
+
+  
   return (
     <div className="App">
       <NavBar />
