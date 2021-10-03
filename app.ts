45a1bedcdd3e7cd1608b620686166e05720edd75
kaori <kaori@weseek.co.jp>
2021-10-03 22:08:22 +0900

    build client and express server with yarn dev command

--- app.ts
+++ app.ts
@@ -4,4 +4,4 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello Worldddd!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3001, () => console.log('Example app listening on port 3001!'))
