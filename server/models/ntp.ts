b992a524fd9a903dbe3984cecca9e303e9bf7621
kaori <kaori@weseek.co.jp>
2021-10-09 02:42:36 +0900

    define utp model

---
+++ server/models/ntp.ts
@@ -0,0 +1,14 @@
+import mongoose, { Schema } from 'mongoose';
+import {NtpDocument, NtpModel}  from '../interfaces/ntp'
+
+
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const NtpSchema = new Schema<NtpDocument, NtpModel>({
+    title: { type: String, required: true },
+    user: { type: ObjectId, required: true },
+    createdAt: { type: Date },
+    updatedAt: { type: Date },
+});
+
+export default mongoose.model('Ntp', NtpSchema);
